3c52381b9602ee50b717a11a9178aedd6b0798ff
jaegwans <skhu201811140@gmail.com>
2023-06-26 15:27:40 +0900

    chore: styled-components setting

---
+++ src/styles/GlobalStyle.tsx
@@ -0,0 +1,40 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    body, html {
+        width: 100%;
+        height: 100%;
+    }
+    body {
+        font-family: 'Noto Sans KR', sans-serif;
+        font-size: 16px;
+        color: #333;
+    }
+    a {
+        text-decoration: none;
+        color: inherit;
+    }
+    button {
+        border: none;
+        outline: none;
+        background-color: transparent;
+        cursor: pointer;
+    }
+    input {
+        border: none;
+        outline: none;
+    }
+    ul, ol, li {
+        list-style: none;
+    }
+    h1, h2, h3, h4, h5, h6 {
+        font-family: 'Noto Sans KR', sans-serif;
+    }
+
+`;
+export default GlobalStyle;
