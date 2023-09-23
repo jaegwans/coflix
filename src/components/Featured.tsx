b780f3a582c5e3f9635cfb84017328133685c1d5
jaegwans <skhu201811140@gmail.com>
2023-09-23 17:58:00 +0900

    feat: featured

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -2,13 +2,50 @@ import React from 'react';
 import styled from 'styled-components';
 
 function Featured() {
-    return <StyledFeatured>Featured</StyledFeatured>;
+    return (
+        <a
+            href="https://www.acmicpc.net/problem/1759"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            <StyledFeatured>
+                <PSWrapper>
+                    <h2 className="title">오늘의 코테 문제</h2>
+                    <div className="ps">
+                        <h3>암호만들기</h3>
+                        <p>1759</p>
+                    </div>
+                    <div>정답률:44.6%</div>
+                </PSWrapper>
+            </StyledFeatured>
+        </a>
+    );
 }
 
 export default Featured;
 
+const PSWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    .title {
+    }
+    .ps {
+        display: flex;
+        align-items: flex-start;
+        gap: 0.3rem;
+    }
+    justify-content: space-around;
+`;
+
 const StyledFeatured = styled.div`
+    cursor: pointer;
+    justify-content: center;
+    align-items: center;
     height: 12.5rem;
-    background-color: grey;
+    background-image: url('/images/ps-back.jpg');
+    background-size: cover;
     width: 100%;
+    display: flex;
+    padding-top: ${({ theme }) => theme.len.tabHeight};
 `;
