bb899b70c044ccb38749f4be38c95f8d14016283
jaegwans <skhu201811140@gmail.com>
2023-09-26 13:58:04 +0900

    chore: recoil err

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,9 @@ import { RecoilRoot } from 'recoil';
 import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import React from 'react';
+import { RecoilEnv } from 'recoil';
+
+RecoilEnv.RECOIL_DUPLICATE_ATOM_KEY_CHECKING_ENABLED = false;
 
 export default function App({ Component, pageProps }: AppProps) {
     const [queryClient] = React.useState(() => new QueryClient());
