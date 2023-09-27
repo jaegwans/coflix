1e9a9fa2778141d4cb7a6575e0b0508ce0fd9fbc
jaegwans <skhu201811140@gmail.com>
2023-09-27 15:07:45 +0900

    fix: info -> resume

--- src/components/common/Tab.tsx
+++ src/components/common/Tab.tsx
@@ -13,7 +13,7 @@ function Tab() {
                 </li>
                 <li onClick={() => onPush('/intro')}>intro</li>
                 <li onClick={() => onPush('/edit')}>edit</li>
-                <li onClick={() => alert('준비중입니다.')}>info</li>
+                <li onClick={() => alert('준비중입니다.')}>resume</li>
             </ul>
         </StyledTab>
     );
